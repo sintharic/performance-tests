1a880a5bc6adb1e2b9ee0fc7f7a209ec71d25707
sintharic <chris297@gmx.de>
2024-01-26 00:28:37 +0100

    implemented the same 2D FFT test in Julia. Seems to be 25% faster but using 200% more memory.

---
+++ julia/main.jl
@@ -0,0 +1,106 @@
+using FFTW, LinearAlgebra, DelimitedFiles
+
+# You can switch between using Intel MKL's FFT and FFTW (default):
+#FFTW.set_provider!("mkl")
+#FFTW.set_provider!("fftw")
+# MKL is ~30% faster on my Macbook!
+
+
+
+
+function setup_memuse_tracker()
+  tracker = Ref(0)
+  function mem_use(tracker)
+    finalizer(mem_use, tracker)
+    println("# Memory used: ", Sys.maxrss()/2^20, " MiB")
+    nothing
+  end
+  finalizer(mem_use, tracker)
+  nothing
+end
+# setup_memuse_tracker()
+
+
+
+function with_dummies(n::Int64 = 1024)
+  nx = n
+  ny = n
+  nyHP1 = ny÷2+1
+  nxHP1 = nx÷2+1
+  dataRx = zeros(Float64, (nx, ny))
+  dataRy = similar(dataRx)
+  dataFx = zeros(ComplexF64,nxHP1,ny)
+  dataFy = similar(dataFx)
+  dummyF = similar(dataFx)
+  dummyR = similar(dataRx)
+
+  rfft = plan_rfft(dummyR)
+  irfft = plan_irfft(dummyF, nx)
+  for i = 0:(n/4 - 1)
+    iq::Int64 = 1 + 2*i
+    prefac::Float64 = 1/(iq*iq)
+    iq = iq + 1
+    if (i%2 == 0) 
+      prefac = -prefac
+    end
+    dataFx[1,iq] = dataFx[1,iq] + im*prefac
+    dataFx[1,ny+2-iq] = dataFx[1,ny+1-iq] - im*prefac
+    dataFy[iq,1] = dataFy[iq,1] + im*prefac
+    dummyF = dataFx
+    dataRx = irfft * dummyF
+    dummyF = dataFy
+    dataRy = irfft * dummyF
+
+    dataFx = rfft * dataRx
+    dataFy = rfft * dataRy
+  end
+
+  # writedlm("triangular_wave_x.dat", dataRx)
+  # writedlm("triangular_wave_y.dat", dataRy)
+end
+
+
+
+function without_dummies(n::Int64 = 1024)
+  nx = n
+  ny = n
+  nyHP1 = ny÷2+1
+  nxHP1 = nx÷2+1
+  dataRx = zeros(Float64, nx, ny)
+  dataRy = similar(dataRx)
+  dataFx = zeros(ComplexF64, nxHP1, ny)
+  dataFy = similar(dataFx)
+
+  rfft = plan_rfft(dataRx)
+  irfft = plan_irfft(dataFx, nx)
+  for i = 0:(n/4 - 1)
+    iq::Int64 = 1 + 2*i
+    prefac::Float64 = 1/(iq*iq)
+    iq = iq + 1
+    if (i%2 == 0) 
+      prefac = -prefac
+    end
+    dataFx[1,iq] = dataFx[1,iq] + im*prefac
+    dataFx[1,ny+2-iq] = dataFx[1,ny+1-iq] - im*prefac
+    dataFy[iq,1] = dataFy[iq,1] + im*prefac
+    dataRx = irfft * dataFx
+    dataRy = irfft * dataFy
+    dataFx = rfft * dataRx
+    dataFy = rfft * dataRy
+  end
+
+  # writedlm("triangular_wave_x.dat", dataRx)
+  # writedlm("triangular_wave_y.dat", dataRy)
+end
+
+@time without_dummies(1024)
+
+# time for multiple resolutions
+# resols = (2*ones(7)) .^ LinRange(4,10,7)
+# @time without_dummies(8)
+# for i = 1 : 7
+#   f = resols[i]
+#   n::Int64 = trunc(f)
+#   println(n)
+#   @time without_dummies(n)
+# end
